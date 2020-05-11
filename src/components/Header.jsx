5e528d815d1ec104e0157b2262ef1c598295eaca
4a <furryforay@gmail.com>
2020-05-10 22:29:30 -0700

    Fix header hyperlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,15 @@ export const Header = () => {
                 <img alt="Fightan Vidya" src={`${process.env.REACT_APP_MEDIA_PATH}IS/FVLogo.png`} />
             </a>
             <nav className="menu">
-                <a href="stream">
+                <a href="/stream">
                     <img alt="Stream" src={`${process.env.REACT_APP_MEDIA_PATH}IS/STREAM.png`} />
                 </a>
                 <img alt="/" src={`${process.env.REACT_APP_MEDIA_PATH}IS/DIV.png`} />
-                <a href="wiki">
+                <a href="/wiki">
                     <img alt="Wiki" src={`${process.env.REACT_APP_MEDIA_PATH}IS/WIKI.png`} />
                 </a>
                 <img alt="/" src={`${process.env.REACT_APP_MEDIA_PATH}IS/DIV.png`} />
-                <a href="betmain">
+                <a href="/betmain">
                     <img alt="Bets" src={`${process.env.REACT_APP_MEDIA_PATH}IS/BETS.png`} />
                 </a>
             </nav>
